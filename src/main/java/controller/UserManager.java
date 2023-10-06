c273b0ccea362ffcc77d58c88a5b360e00baf087
Duc Anh <anhduong764027@gmail.com>
2023-10-06 09:08:21 +0700

    User Management

---
+++ src/main/java/controller/UserManager.java
@@ -0,0 +1,59 @@
+package controller;
+
+import common.Algorithm;
+import common.Validation;
+import model.User;
+import view.Menu;
+public class UserManager {
+    private final String[] MAIN_MENU_ITEMS = {
+        " Create a new account",
+        " Login System",
+        " Exit",};
+
+    Algorithm algorithm = new Algorithm();
+    Validation validation = new Validation();
+
+    Menu mainMenu;
+
+    public UserManager() {
+        this.mainMenu = new Menu("====== USER MANAGEMENT SYSTEM ======", MAIN_MENU_ITEMS) {
+            @Override
+            public void execute(int choice) {
+                switch (choice) {
+                    case 1:
+                        algorithm.createNewAccount();
+                        break;
+                    case 2:
+                        loginSystem();
+                        break;
+                    case 3:
+                        System.exit(0);
+                        break;
+                        
+                }
+            }
+            
+            public void loginSystem() {
+                if (!validation.checkFileExist()) {
+                    return;
+                }
+                User user = new User();
+                user.setUserName(validation.checkInputUsername());
+                user.setPassWord(validation.checkInputPassword());
+                if (!validation.checkUsernameExist(user.getUserName())) {
+                    if (!user.getPassWord().equalsIgnoreCase(algorithm.passwordByUsername(user.getUserName()))) {
+                        System.err.println("Password incorrect.");
+                    } else {
+                        System.err.println("Login success.");
+                    }
+                } else {
+                    System.err.println("Username incorrect.");
+                }
+            }
+        };
+    }
+
+    public void run() {
+        mainMenu.run();
+    }
+}
---
+++ src/main/java/test/Mian.java
@@ -0,0 +1,11 @@
+
+
+package test;
+import controller.UserManager;
+
+public class Mian {
+
+    public static void main(String[] args) {
+        new UserManager().run();
+    }
+}
